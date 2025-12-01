666a87d3308370832002df8cf1d92cf74cca4f3c
siddharthsahu1211-ops <siddharthsahu1211@gmail.com>
2025-12-01 09:55:37 +0000

    finish cCRUD opertion

--- router.py
+++ router.py
@@ -5,13 +5,12 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse
 
 from controllers.students import (
-    get_all_students,
-    get_student,
-    create_student,
-    # , update_student
-    # , delete_student
+    get_all_students
+    , get_student
+    , create_student
+    , update_student
+    , delete_student
 )
-
 from core.static import serve_static
 from core.responses import send_404
 from core.middleware import add_cors_headers
@@ -60,4 +59,15 @@ class StudentRouter(BaseHTTPRequestHandler):
         if self.path == "/api/students":
             return create_student(self)
         return send_404(self)
+    def do_PUT(self):
+        if self.path.startswith("/api/students/"):
+            student_id = int(self.path.split("/")[-1])
+            return update_student(self, student_id)
+        return send_404(self)
+    def do_delete(self):
+        if self.path.statswitch("/api/students?"):
+            student_id=int(self.path.split("/")[-1])
+            return delete_student(self,student_id)
+        return send_404(self)
     
+  
